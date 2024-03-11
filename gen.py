d58ac497dfb1b496d415680bb3a973f7a98c8bb2
Quentin Santos <qsantos@qsantos.fr>
2024-03-11 18:10:37 +0100

    Basic page generation from entries

---
+++ gen.py
@@ -0,0 +1,26 @@
+import string
+import random
+
+
+def main() -> None:
+    with open('2024-03-11') as f:
+        challenge, ruby, choice1, choice2, choice3, choice4, *meaning = f.read().strip().split('\n')
+
+    choice1 = f'<button class="choice" data-correct><div>{choice1}</div></button>'
+    choice2 = f'<button class="choice"><div>{choice2}</div></button>'
+    choice3 = f'<button class="choice"><div>{choice3}</div></button>'
+    choice4 = f'<button class="choice"><div>{choice4}</div></button>'
+    choices = [choice1, choice2, choice3, choice4]
+    random.shuffle(choices)
+    [choice1, choice2, choice3, choice4] = choices
+    meaning = ''.join(meaning)
+
+    with open('template.html') as f:
+        template = f.read()
+    html = string.Template(template).substitute(template, **locals())
+    with open('index.html', 'w') as f:
+        f.write(html)
+
+
+if __name__ == '__main__':
+    main()
