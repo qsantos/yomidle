3b94153b449653b74aeb176da0e3961b7cb7ae43
Quentin Santos <qsantos@qsantos.fr>
2024-03-12 07:32:36 +0100

    Cycle through the challenges depending on the day

--- gen.py
+++ gen.py
@@ -1,9 +1,15 @@
+from datetime import date
+from glob import glob
 import string
 import random
 
+START = date(2024, 3, 11)
+
 
 def main() -> None:
-    with open('challenges/000') as f:
+    challenges = glob('challenges/*')
+    delta = (date.today() - START).days % len(challenges)
+    with open(f'challenges/{delta:03}') as f:
         challenge, ruby, choice1, choice2, choice3, choice4, *meaning = f.read().strip().split('\n')
 
     choice1 = f'<button class="choice" data-correct><div>{choice1}</div></button>'
